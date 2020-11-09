936c624d445b9b23b28864bdc07b2ee9ed0fa042
Fredomontero <camontero@tekmexico.com>
2020-11-08 22:48:12 -0800

    Add promise test example

---
+++ src/utils/exe.js
@@ -0,0 +1,13 @@
+const fetchUser = () => {
+  let time = 1000;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        name: 'Alfredo Martinez',
+        age: 29,
+      });
+    }, time);
+  });
+};
+
+fetchUser().then(console.log);
